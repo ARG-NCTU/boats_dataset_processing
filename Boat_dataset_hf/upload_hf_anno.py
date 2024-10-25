9d1024478edf3970b28580dc3e9f1623d36edca2
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2024-10-25 15:34:38 +0800

    check and upload hf annotation

--- Boat_dataset_hf/upload_hf_anno.py
+++ Boat_dataset_hf/upload_hf_anno.py
@@ -5,12 +5,3 @@ os.system('huggingface-cli upload ARG-NCTU/Boat_dataset_2024 classes.txt data/cl
 for anno_files in os.listdir("annotations"):
     if anno_files.endswith(".jsonl"):
         os.system(f'huggingface-cli upload ARG-NCTU/Boat_dataset_2024 annotations/{anno_files} data/{anno_files} --repo-type=dataset --commit-message="Upload labels to hub"')
-
-# Make files with urls
-URL = "https://huggingface.co/datasets/ARG-NCTU/Boat_dataset_2024/resolve/main"
-with open("urls.txt", "w") as f:
-    for anno_files in os.listdir("annotations"):
-        if anno_files.endswith(".jsonl"):
-            f.write(f"{URL}/data/{anno_files}\n")
-    f.write(f"{URL}/data/classes.txt\n")
-os.system('huggingface-cli upload ARG-NCTU/Boat_dataset_2024 urls.txt data/urls.txt --repo-type=dataset --commit-message="Upload urls to hub"')
