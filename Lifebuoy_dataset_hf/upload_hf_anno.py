65a7082863d7cfd99d0c296e92df708609612122
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2024-10-25 15:58:40 +0800

    update Lifebuoy hf code

---
+++ Lifebuoy_dataset_hf/upload_hf_anno.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('huggingface-cli upload ARG-NCTU/Lifebuoy_dataset_2024 classes.txt data/classes.txt --repo-type=dataset --commit-message="Upload classes list to hub"')
+
+for anno_files in os.listdir("annotations"):
+    if anno_files.endswith(".jsonl"):
+        os.system(f'huggingface-cli upload ARG-NCTU/Lifebuoy_dataset_2024 annotations/{anno_files} data/{anno_files} --repo-type=dataset --commit-message="Upload labels to hub"')
+
