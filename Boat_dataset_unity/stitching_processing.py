a3da71550ecc6d3d317d95f95492cd53e85c0321
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2025-01-25 22:27:13 +0800

    fix(stitching): correct file paths and add homography paths
    - Corrected the spelling of "stiching" to "stitching" in file paths.
    - Added homography paths (h1_path and h2_path) to the stitching script.
    - Updated the os.system command to include the new homography paths.

--- Boat_dataset_unity/stitching_processing.py
+++ Boat_dataset_unity/stitching_processing.py
@@ -2,12 +2,14 @@ import os
 from tqdm import tqdm
 
 # h1 h2
-# os.makedirs("stiched_results/h1_h2", exist_ok=True)
+# os.makedirs("stitched_results/h1_h2", exist_ok=True)
 # os.system("python stiching.py --input_dir Images/boats14-22/Scene1 --output_dir stiched_results/h1_h2")
 
 input_root = "Images"
 output_root = "stiched_results"
 suffixes = ["", "_depth", "_seg", "_thermal"]
+h1_path = "stitched_results/h1_h2/homography/H1_1.npy"
+h2_path = "stitched_results/h1_h2/homography/H2_1.npy"
 
 for boat_folder in tqdm(os.listdir(sorted(input_root)), desc="Boat Folder", leave=False):
     boat_folder_path = os.path.join(input_root, boat_folder)
@@ -17,6 +19,6 @@ for boat_folder in tqdm(os.listdir(sorted(input_root)), desc="Boat Folder", leav
         os.makedirs(os.path.join(output_root, boat_folder, scene_folder), exist_ok=True)
         output_path = os.path.join(output_root, boat_folder, scene_folder)
         for suffix in tqdm(suffixes, desc="Suffix", leave=False):
-            os.system(f"python stiching.py --input_dir {scene_folder_path} --suffix {suffix} --output_dir {output_path}")
+            os.system(f"python stiching.py --input_dir {scene_folder_path} --suffix {suffix} --output_dir {output_path} --h1_path {h1_path} --h2_path {h2_path}")
         
         
