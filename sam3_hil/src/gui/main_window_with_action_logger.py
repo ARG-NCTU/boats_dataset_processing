8bac1dcfbd3bd1b9aeb887124a72804d7d85732d
AdamShih27 <scy.en13@nycu.edu.tw>
2026-03-05 14:51:22 +0800

    feat: simplify menu action labels by removing emojis

--- sam3_hil/src/gui/main_window_with_action_logger.py
+++ sam3_hil/src/gui/main_window_with_action_logger.py
@@ -1038,7 +1038,7 @@ class HILAAMainWindow(QMainWindow):
         prompt_layout = QHBoxLayout()
         prompt_layout.addWidget(QLabel("Prompt:"))
         from PyQt6.QtWidgets import QLineEdit
-        self.prompt_input = QLineEdit("boat, ship")
+        self.prompt_input = QLineEdit("boat")
         prompt_layout.addWidget(self.prompt_input)
         settings_layout.addLayout(prompt_layout)
         
@@ -1272,26 +1272,26 @@ class HILAAMainWindow(QMainWindow):
         menu = QMenu(self)
         
         # 刪除物件
-        delete_action = menu.addAction("🗑️ Delete Object (All Frames)")
+        delete_action = menu.addAction("Delete Object (All Frames)")
         delete_action.triggered.connect(lambda: self.delete_object(obj_id))
         
-        delete_from_action = menu.addAction("🗑️ Delete From Current Frame")
+        delete_from_action = menu.addAction("Delete From Current Frame")
         delete_from_action.triggered.connect(lambda: self.delete_object_from_frame(obj_id, self.current_frame))
         
         menu.addSeparator()
         
         # 合併物件
-        merge_action = menu.addAction("🔗 Merge Into Another Object...")
+        merge_action = menu.addAction("Merge Into Another Object...")
         merge_action.triggered.connect(lambda: self.show_merge_dialog(obj_id))
         
         # 交換標籤
-        swap_action = menu.addAction("🔄 Swap Label With...")
+        swap_action = menu.addAction("Swap Label With...")
         swap_action.triggered.connect(lambda: self.show_swap_dialog(obj_id))
         
         menu.addSeparator()
         
         # 跳轉到物件首次出現的幀
-        jump_action = menu.addAction("📍 Jump to First Appearance")
+        jump_action = menu.addAction("Jump to First Appearance")
         jump_action.triggered.connect(lambda: self.jump_to_object_first_frame(obj_id))
         
         # 顯示選單
